6f563d1bd224fa0a8221943c34a4aada6ce6b199
Fabricio Jonatan <110259064+FabricioJonatan@users.noreply.github.com>
2023-08-08 23:08:14 -0300

    Inicio do Projeto, fazendo limpeza e montando base
    Foi feito a produção de um projeto React para uma página To Do e após foi feito a preparação do local para receber os códigos e iniciado o projeto.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useState, useEffect} from 'react'
+import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from 'react-icons/bs'
+
+const API = 'http://localhost:5000'
+
 function App() {
+  const [title, setTitle] = useState("")
+  const [time, setTime] = useState("")
+  const [toDo, setToDo] = useState([])
+  const [loading, setLoading] = useState(false)
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		console.log(title, time);
+	}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<div className="todo-header">
+				<h1>React To Do</h1>
+			</div>
+			<div className="form-todo">
+				<h2>Insira Sua Próxima Tarefa : </h2>
+				<form onSubmit={handleSubmit}>
+					<div className="form-control">
+						<label htmlFor="title">O que você fará?</label>
+						<input
+							type="text"
+							name="title"
+							placeholder='Titulo da Tarefa'
+							onChange={(e) => {setTitle(e.target.value)}}
+							value={title || ''}
+							required
+						/>
+					</div>
+					<div className="form-control">
+						<label htmlFor="title">Duração : </label>
+						<input
+							type="number"
+							name="time"
+							placeholder='Duração da Tarefa (em horas)'
+							onChange={(e) => {setTime(e.target.value)}}
+							value={time || ''}
+							required
+						/>
+					</div>
+					<input type="submit" value="Adicionar Tarefa"/>
+				</form>
+			</div>
+			<div className="todo-list">
+				<h2>Lista De Tarefas :</h2>
+				{toDo.length === 0 && <p>Não há itens na lista ainda!</p>}
+			</div>
     </div>
   );
 }
